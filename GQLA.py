c5b136aba394956cf7c7672a92cf247e1ebc9a37
Alenstoir <alenstoir@yandex.ru>
2020-09-10 12:56:30 +0300

    Removed junk code

--- GQLA.py
+++ GQLA.py
@@ -267,227 +267,25 @@ def parse_object(item):
     return object_instance
 
 
-async def asynchronous():
-    helper = GQLA('ship')
-    helper.url = '10.10.127.19'
-    helper.port = '8086'
-
-    ignore = ['pageInfo', 'deprecationReason', 'isDeprecated', 'cursor']
-
-    helper.set_ignore(ignore)
-
-    await helper.introspection()
-
-    logging.info(' '.join(['QUERRY vesselTypes RESULT:', str(await helper.query_one('vesselTypes', to_file=True))]))
-    logging.info(' '.join(['QUERRY vesselTypes RESULT:', str(await helper.query_one('civilVesselsData', to_file=True, onlyIdentified='false', first=1))]))
-    logging.info(' '.join(['QUERRY vesselTypes RESULT:', str(await helper.query_one('shipData', to_file=True, onlyIdentified='false', first=20))]))
-
-
-if __name__ == "__main__":
-    loop_ = asyncio.get_event_loop()
-    loop_.run_until_complete(asynchronous())
-
-    loop_.close()
-    pass
-
-# URL_DEVEL = "http://10.10.127.19:8088/graphql"
-# MAX_CLIENTS = 1
-
-
-# SUBQUERY = "{subquery_name} {{ {subquery_fields} }}"
-# FLAG = SUBQUERY.format(subquery_name='flag', subquery_fields='ksm shortName name alfa2 alfa3')
-# COUNTRY = SUBQUERY.format(subquery_name='country', subquery_fields='ksm shortName name alfa2 alfa3')
-# VESSEL_TYPE = SUBQUERY.format(subquery_name='vesselType', subquery_fields='vesselTypeEn vesselTypeRu')
-# FLEET = SUBQUERY.format(subquery_name='fleet', subquery_fields='fcUuid abbreviation name region')
-# KEUZ = SUBQUERY.format(subquery_name='keuzClassifier', subquery_fields='clsfObjId relationTypeId kodKeuz')
-# PROJECT = SUBQUERY.format(subquery_name='project', subquery_fields='fc_Uuid TKPrjtthCode TKPrjtthKodprj TKPrjtthName TKPrjtthVmfcl TKPrjtthIgcl TKPrjtthDl TKPrjtthSh TKPrjtthOs TKPrjtthVistd TKPrjtthVipln TKPrjtthVin TKPrjtthVip TKPrjtthGrp TKPrjtthSpn TKPrjtthSpp TKPrjtthDaln TKPrjtthDalp TKPrjtthGl TKPrjtthCh TKPrjtthAvt TKPrjtthVoor TKPrjtthRgav  TKPrjtthDes TKPrjtthPrim TKPrjtthBegda TKPrjtthEndda TKPrjtthPr')
-# MODEL3D = SUBQUERY.format(subquery_name='model3d', subquery_fields='urlImg urlModel urlWaterlineModel urlWaterlineImg swiftUrlModel swiftUrlWaterlineModel swiftUrlImg swiftUrlWaterlineImg fileModel fileWaterlineModel fileImg fileWaterlineImg')
-# QUERY_RAW = """
-#                 query {{
-#                     {query} {{
-#                         {fields}
-#                         {subquery}
-#                     }}
-#                 }}
-#             """
+# async def asynchronous():  # Пример работы
+#     helper = GQLA('ship')
+#     helper.url = '10.10.127.19'
+#     helper.port = '8086'
 #
-# CIVIL_FIELDS = ['imo',
-#                 'mmsi',
-#                 'name',
-#                 'formerNames',
-#                 'operatingStatus',
-#                 'grossTonnage',
-#                 'deadweight',
-#                 'length',
-#                 'breadth',
-#                 'length',
-#                 'engineType',
-#                 'engineModel',
-#                 'enginePower',
-#                 'yearOfBuild',
-#                 'builder',
-#                 'classificationSociety',
-#                 'homePort',
-#                 'owner',
-#                 'manager',
-#                 'vesselTypeId',
-#                 'ksm',
-#                 'captain',
-#                 VESSEL_TYPE,
-#                 FLAG]
-# MILITARY_FIELDS = ['fc_Uuid',
-#                    'TKKplvmfCode',
-#                    'TKKplvmfName',
-#                    'TKKplvmfNamec',
-#                    'TKKplvmfNom',
-#                    'TKKplvmfPpb',
-#                    'TKKplvmfDatvv',
-#                    'TKKplvmfSost',
-#                    'TKKplvmfBegda',
-#                    'TKKplvmfEndda',
-#                    'TKKplvmfPr',
-#                    'TKKplvmfFl',
-#                    'TKKpligKsm',
-#                    'TKKpligNameclat',
-#                    'TKKpligCodealt',
-#                    'TKKplvmfInd',
-#                    'TKPrjtthCode',
-#                    'TKPrjtthVmfcl',
-#                    'commonClassifierId',
-#                    'relationTypeId',
-#                    'nameShort',
-#                    'abbreviation',
-#                    'serviceTime',
-#                    'technicalReadinessName',
-#                    'technicalReadinessEvent',
-#                    'alertName',
-#                    'alertWhoseOrder',
-#                    'alertOrderNum',
-#                    'alertOrderDate',
-#                    FLEET,
-#                    COUNTRY,
-#                    PROJECT,
-#                    KEUZ,
-#                    MODEL3D]
-
-# def civil_query():
-#     civil_fields = (' '.join(CIVIL_FIELDS))
-#     civil_node = SUBQUERY.format(subquery_name='node', subquery_fields=civil_fields)
-#     civil_edge = SUBQUERY.format(subquery_name='edges', subquery_fields=civil_node)
-#     return {'query': QUERY_RAW.format(query=' civilVesselsData(onlyIdentified: false)',
-#                                       fields='', subquery=civil_edge)}
-
-
-# def military_query():
-#     military_fields = (' '.join(MILITARY_FIELDS))
-#     military_node = SUBQUERY.format(subquery_name='node', subquery_fields=military_fields)
-#     military_edge = SUBQUERY.format(subquery_name='edges', subquery_fields=military_node)
-#     return {'query': QUERY_RAW.format(query=' shipData(onlyIdentified: false)',
-#                                       fields='', subquery=military_edge)}
-
-
-# async def introspection(url, ignore):
-#     queries = []
-#     futures = [fetch_async(0, url, INTROSPECTION)]
-#     done, pending = await asyncio.wait(futures)
-#     result = done.pop().result()
-#     queries = result['data']['__schema']['types']
-#     # print(result)
-#     with open('query.json', 'w') as ofs:
-#         ofs.write(json.dumps(queries, indent=4))
-#     # for item in queries:
-#     #     print(item)
-#     model = create_data(queries, ignore)
-#     generate_queries(model)
-#     return queries
-
-# def subquery(item, vault, ignore):
-#     query = []
-#     for field in item.fields:
-#         if item.fields[field].kind == "OBJECT":
-#             if field in ignore:
-#                 continue
-#             subquery_val = item.fields[field].name
-#             subquery_val = vault.objects[subquery_val]
-#             subquery_val = subquery(subquery_val, vault, ignore)
-#             query.append((str(field) + ' {' + ' '.join(subquery_val) + '}'))
-#         else:
-#             query.append(field)
-#     return query
-
-
-# def generate_queries(model, specific=False):
-#     ignore = model._ignore
-#     if 'Query' in model.objects:
-#         queries = model.objects['Query'].fields
-#     elif 'Queries' in model.objects:
-#         queries = model.objects['Queries'].fields
-#     else:
-#         raise NotImplementedError
-#     query_str = {}
-#     for query in queries:
-#         if queries[query].kind == 'OBJECT':
-#             subquery_val = subquery(model.objects[queries[query].name], model, ignore)
-#             query_str[query] = ' {' + ' '.join(subquery_val) + '}'
-#             # print(model.objects[queries[query].name])
-#             # print(query, queries[query])
-#     for entry in query_str:
-#         print(entry, query_str[entry])
-
-
-# def create_data(data, ignore):
-#     # print(data)
-#     model = GQModel()
-#     model.set_ignore(ignore)
-#     for item in data:
-#         if item['kind'] == 'ENUM':
-#             model.add_enum(parse_enum(item))
-#         elif item['kind'] == 'SCALAR':
-#             model.add_scalar(parse_scalar(item))
-#         elif item['kind'] == 'OBJECT':
-#             obj = parse_object(item)
-#             # print(repr(obj))
-#             model.add_object(obj)
-#     return model
-
-
-# args = []
-# subqueries = []
-# print(query['name'])
-# for item in query['args']:
-#     args.append([item['name'], item['type']])
-# for item in query['type']:
-#     subqueries.append(item)
-# print(query)
-
-
-# async def create_query(query):
-#     print(json.dumps(query, indent=4))
-#     args = []
-#     subqueries = []
-#     print(query['name'])
-#     for item in query['args']:
-#         args.append([item['name'], item['type']])
-#     for item in query['type']:
-#         subqueries.append(item)
-#     print(query)
-
-
-# async def fetch_async(pid, url, query):
-#     print('Fetch async process {} started'.format(pid))
-#     async with aiohttp.request('POST', url, json=query) as resp:
-#         response = await resp.text()
-#     return json.loads(response)
-
-
-# async def asynchronous(type_):
-#     civil_string = civil_query()
-#     military_string = military_query()
-#     results = []
-#     futures = [fetch_async(i, URL_DEVEL, civil_string if type_ == 'civ' else military_string) for i in
-#                range(1, MAX_CLIENTS + 1)]
-#     done, pending = await asyncio.wait(futures)
-#     for i in range(1, MAX_CLIENTS + 1):
-#         results.append(done.pop().result())
-#     print(results)
-#     return results
+#     ignore = ['pageInfo', 'deprecationReason', 'isDeprecated', 'cursor']
+#
+#     helper.set_ignore(ignore)
+#
+#     await helper.introspection()
+#
+#     logging.info(' '.join(['QUERRY vesselTypes RESULT:', str(await helper.query_one('vesselTypes', to_file=True))]))
+#     logging.info(' '.join(['QUERRY vesselTypes RESULT:', str(await helper.query_one('civilVesselsData', to_file=True, onlyIdentified='false', first=1))]))
+#     logging.info(' '.join(['QUERRY vesselTypes RESULT:', str(await helper.query_one('shipData', to_file=True, onlyIdentified='false', first=20))]))
+#
+#
+# if __name__ == "__main__":
+#     loop_ = asyncio.get_event_loop()
+#     loop_.run_until_complete(asynchronous())
+#
+#     loop_.close()
+#     pass
