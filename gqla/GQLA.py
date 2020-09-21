32f5b33948ed66651aa96e142f93e4aadfa491e2
Alenstoir <alenstoir@yandex.ru>
2020-09-21 13:27:34 +0300

    Enhanced SOLID

--- gqla/GQLA.py
+++ gqla/GQLA.py
@@ -1,11 +1,11 @@
 import asyncio
 import json
 
-import aiohttp
 import logging
 import logging.config
 import os.path
 
+from gqla.Executor.Executor import BasicExecutor
 from gqla.GQLModel.GQLModel import GQModel
 from gqla.GQLStorage.GQLStorage import TypeFactory
 from gqla.GQQStorage.GQQStorage import BasicStorage
@@ -13,15 +13,7 @@ from gqla.GQQStorage.GQQStorage import BasicStorage
 
 class GQLA:
     __slots__ = ('url', 'port', 'name', '_ignore', '_model', '_queries', '_subpid', 'usefolder', 'recursive_depth',
-                 '_depth', 'qStorage')
-
-    URL_TEMPLATE = "http://{}:{}/graphql"
-
-    QUERY_RAW = """
-                    query {{
-                        {query}
-                    }}
-                """
+                 '_depth', 'qStorage', 'executor')
 
     INTROSPECTION = {
         'query': '\n    query IntrospectionQuery {\n      __schema {\n        queryType { name }\n        '
@@ -54,43 +46,17 @@ class GQLA:
         self.usefolder = usefolder
         self.recursive_depth = recursive_depth
         self.qStorage = None
+        self.executor = BasicExecutor(url, port, self.qStorage)
 
         logging.info(' '.join(['CREATED', 'CLASS', str(self.__class__)]))
 
     def set_ignore(self, ignore_):
         self._ignore = ignore_
 
-    def _can_query(self):
-        if self.url is None or self.port is None or self.name is None:
-            raise AttributeError
-
-    @staticmethod
-    async def fetch_async(pid, url, query):
-        logging.info('Fetch async process {} started'.format(pid))
-        async with aiohttp.request('POST', url, json=query) as resp:
-            response = await resp.text()
-        logging.info('Fetch async process {} ended'.format(pid))
-        return json.loads(response)
-
     async def query_one(self, query_name, to_file=False, **kwargs):
-        instance = self.qStorage.storage[query_name]
-        self._can_query()
-        logging.info(' '.join(['QUERRYING', query_name, 'WITH PARAMS', str(kwargs)]))
-        if len(kwargs) > 0:
-            params = "(" + str(kwargs).replace("'", '').replace('{', '').replace('}', '') + ")"
-        else:
-            params = ''
-        instance.args(params)
-        query = {
-            'query': self.QUERY_RAW.format(query=instance.query)
-        }
-        logging.info(query)
 
-        futures = [self.fetch_async(self._subpid, self.URL_TEMPLATE.format(self.url, self.port), query=query)]
-        self._subpid += 1
+        result = await self.executor.execute(self._subpid, query_name)
 
-        done, pending = await asyncio.wait(futures)
-        result = done.pop().result()
         if self.usefolder:
             if to_file:
                 folder = os.path.join('', self.name)
@@ -103,24 +69,9 @@ class GQLA:
         return result
 
     async def introspection(self):
-        self._can_query()
-
         logging.info(' '.join(['QUERRYING', self.name, 'INTROSPECTION']))
-
-        futures = [self.fetch_async(self._subpid, self.URL_TEMPLATE.format(self.url, self.port), self.INTROSPECTION)]
-        self._subpid += 1
-
-        done, pending = await asyncio.wait(futures)
-        result = done.pop().result()
-
+        result = await self.query_one(self.INTROSPECTION)
         queries = result['data']['__schema']['types']
-
-        if self.usefolder:
-            folder = os.path.join('', self.name)
-            if not os.path.exists(folder):
-                os.mkdir(folder)
-            with open(os.path.join(folder, 'model.json'), 'w') as ofs:
-                ofs.write(json.dumps(queries, indent=4))
 
         self.create_data(queries)
         self.generate_queries()
@@ -143,12 +94,11 @@ class GQLA:
 
         for query in queries:
             self.qStorage.create(query, queries[query], self._model, self._ignore, self.recursive_depth)
+        self.executor._storage = self.qStorage
 
 
 async def asynchronous():  # Пример работы
-    helper = GQLA('solar', usefolder=True)
-    helper.url = 'localhost'
-    helper.port = '8080'
+    helper = GQLA('solar', url='localhost', port='8080', usefolder=True)
 
     ignore = ['pageInfo', 'deprecationReason', 'isDeprecated', 'cursor', 'parent1']
 
